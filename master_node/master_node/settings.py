02e16e12021cda272716812b84700a789642fb27
saurabhjinturkar <saurabh.jinturkar@sjsu.edu>
2015-10-14 16:46:09 -0700

    created Build Manager package

--- master_node/master_node/settings.py
+++ master_node/master_node/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'master_node.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'clusterdb',
+        'USER': 'pi',
+        'PASSWORD': 'raspberry',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
