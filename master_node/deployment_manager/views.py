8ca4b6b3159c9fab2779b959ff4e90abfb02c134
Aditya Sharma <adityasharmacs@gmail.com>
2015-11-15 20:03:20 -0800

    1. Urls.py - Changes done for calling the function for Deploying a particular cluster  2. Views.py - deployClusterID for Queuing a particular cluster.

--- master_node/deployment_manager/views.py
+++ master_node/deployment_manager/views.py
@@ -1,3 +1,45 @@
 from django.shortcuts import render
 
+import json
+import http.client
+
+from django.http import HttpResponse
+from django.http import HttpRequest
+
+from django.views.decorators.csrf import csrf_exempt
+from django.views.decorators.http import require_http_methods
+from raspberryq.queuemanager.queue import manager
+
+
 # Create your views here.
+
+#Create class just for the purpose of Static Function
+class StaticClass:
+    clusterID = 1
+
+# Method to get the Cluster ID to deploy  on the Slave Node
+def deployClusterID(request, cluster_id):
+    data = json.dumps({'cluster_id': cluster_id})
+    if(StaticClass.clusterID % 2 == 0):
+        topicData = "Build_Manager_Queue"+(StaticClass.clusterID+1)
+        StaticClass.clusterID = 1
+    else:
+        topicData = "Build_Manager_Queue"+ StaticClass.clusterID
+        StaticClass.clusterID += 1
+
+    params = json.dumps({"topic": topicData, "data": data, "priority": 3})
+    headers = {"Content-type": "application/x-www-form-urlencoded", "Accept": "text/plain"}
+    conn = http.client.HTTPConnection("localhost:4242")
+    conn.request("POST", "/queue/enqueue", params, headers)
+    response = conn.getresponse()
+    print(response.status, response.reason)
+    data = response.read()
+    print(data)
+    conn.close()
+    return HttpResponse(data)
+
+
+
+
+
+
