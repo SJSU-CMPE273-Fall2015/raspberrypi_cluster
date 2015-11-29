2a5a0a24a011f1804f0f5a4e84478699a4b1459f
Sayalee1 <sayalee.agashe@gmail.com>
2015-11-29 13:55:58 -0800

    Added Admin Charts

--- master_node/core/admin.py
+++ master_node/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Cluster, ClusterProject, Project, ProjectAudit, ProjectBuild, SystemAudit
-
+from django.template import loader, Context, RequestContext
+from django.views.decorators.http import require_http_methods
+from django.shortcuts import render_to_response
+from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 # Register your models here.
 
 #admin.site.register(User)
@@ -10,3 +14,10 @@ admin.site.register(Project)
 #admin.site.register(ProjectAudit)
 #admin.site.register(ProjectBuild)
 admin.site.register(SystemAudit)
+
+
+def index(request):
+    p = Project.objects.all()
+    t = loader.get_template('admin/index.html')
+    c = Context({'p': p})
+    return HttpResponse(t.render(c))
--- master_node/core/urls.py
+++ master_node/core/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
 from core.views import *
 from core import views
+from core import admin
 
 urlpatterns = patterns('',
     url(r'^$', 'django.contrib.auth.views.login'),
@@ -9,6 +10,7 @@ urlpatterns = patterns('',
     url(r'^register/$', register),
     url(r'^register/success/$', register_success),
     url(r'^home/$', views.home, name='home'),
+    url(r'^index/$', admin.index, name='index'),
     url(r'^insert/$', views.insert, name='insert'),
     url(r'^delete/(?P<project_id>\d+)$', views.delete, name='delete'),
     url(r'^edit/(?P<project_id>\d+)$', views.edit, name='edit'),
